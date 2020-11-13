1b998a28fc06b97ef9b657f0f4ce007ef1f68fc8
Ruan Scherer <ruan.vscherer@gmail.com>
2020-11-12 21:19:18 -0300

    Add seed for users

---
+++ src/main/java/com/ruanscherer/springboot_mongodb/config/Instantiation.java
@@ -0,0 +1,24 @@
+package com.ruanscherer.springboot_mongodb.config;
+
+import com.ruanscherer.springboot_mongodb.domain.User;
+import com.ruanscherer.springboot_mongodb.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
