533643ee3e0b844aad0d5136a57094ccafa1dbf1
Ruan Scherer <ruan.vscherer@gmail.com>
2020-11-12 21:26:16 -0300

    Add user DTO

--- src/main/java/com/ruanscherer/springboot_mongodb/resources/UserResource.java
+++ src/main/java/com/ruanscherer/springboot_mongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.ruanscherer.springboot_mongodb.resources;
 
 import com.ruanscherer.springboot_mongodb.domain.User;
+import com.ruanscherer.springboot_mongodb.dto.UserDTO;
 import com.ruanscherer.springboot_mongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,9 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController()
 @RequestMapping(value = "/users")
@@ -20,7 +20,9 @@ public class UserResource {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
-        return ResponseEntity.ok().body(userService.findAll());
+    public ResponseEntity<List<UserDTO>> findAll() {
+        List<User> users = userService.findAll();
+        List<UserDTO> usersResponse = users.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(usersResponse);
     }
 }
